a9911697860bcde0caefc0a323b9053bca714652
davi-souza2001 <davisouza2001dv@gmail.com>
2022-01-11 16:49:06 -0300

    starting components message

---
+++ src/components/MessageContent/index.tsx
@@ -0,0 +1,9 @@
+import styles from 'MessageContent.module.css'
+
+export default function index(){
+    return (
+        <div>
+            <h2>Ola</h2>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import TopBar from '../components/TopBar'
 import ListMessages from '../components/ListMessages'
+import MessageContent from '../components/MessageContent'
 
 export default function index() {
   return (
     <>
-      <TopBar />
+      <TopBar /> 
       <ListMessages />
+      <MessageContent/>
     </>
   )
 }
