c9bd8413bf2baccafdf779c58141a6422ab72b77
davi-souza2001 <davisouza2001dv@gmail.com>
2022-01-13 20:38:48 -0300

    start separate top bar

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,32 +1,27 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import styles from './TopBar.module.css'
 
 export default function ButtonAppBar() {
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    <div className={styles.contentGeral}>
+      <div className={styles.boxContentGeral}>
+        <div className={styles.boxContent}>
           ChatSocial
-          </Typography>
-          <Button color="inherit">Profile</Button>
-          <Button color="inherit">Settings</Button>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+        </div>
+        <div className={styles.boxPhoto}>
+          Foto Logado
+        </div>
+      </div>
+      <div className={styles.contentNameUserMessage}>
+        <div>
+          Foto
+        </div>
+        <div>
+          Nome da pessoa
+        </div>
+        <div>
+          País da pessoa
+        </div>
+      </div>
+    </div>
   );
 }
