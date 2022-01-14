0d267879913b4bc6e31cb2cf8223b3fa9d346e29
davi-souza2001 <davisouza2001dv@gmail.com>
2022-01-14 16:12:16 -0300

    box with messages finish

--- src/components/MessageContent/index.tsx
+++ src/components/MessageContent/index.tsx
@@ -1,9 +1,30 @@
 import styles from './MessageContent.module.css'
 
-export default function index(){
+export default function index() {
     return (
         <div className={styles.contentGeral}>
-            <h2>Ola</h2>
+            <div className={styles.contentMessages}>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
