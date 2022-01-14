b233f9e980933ccf58bd1e84a861a8bb5c451817
davi-souza2001 <davisouza2001dv@gmail.com>
2022-01-14 16:18:51 -0300

    up

--- src/components/MessageContent/index.tsx
+++ src/components/MessageContent/index.tsx
@@ -24,7 +24,48 @@ export default function index() {
                         <p>Olassd sadsad sadsad sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa</p>
                     </div>
                 </div>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicUserSelf}>
+                    <div className={styles.messageOutUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Ola</p>
+                    </div>
+                </div>
+                <div className={styles.contentMessagesUnicOutUser}>
+                    <div className={styles.messageWithUser}>
+                        <p>Olassd sadsad sadsad sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa sa asdsadsa</p>
+                    </div>
+                </div>
             </div>
+            <h2>dsa</h2>
         </div>
     )
 }
