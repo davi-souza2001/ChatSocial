d19e5a4347a02694920923837a0de0d97a95d66c
davi-souza2001 <davisouza2001dv@gmail.com>
2022-01-14 20:44:30 -0300

    new height in topbar

--- src/components/ListMessages/index.tsx
+++ src/components/ListMessages/index.tsx
@@ -1,17 +1,15 @@
-import { Fab } from '@mui/material'
 import BoxMessage from '../BoxMessage'
-import AddIcon from '@mui/icons-material/Add'
 
 import styles from './ListMessage.module.css'
 
 export default function index() {
     return (
         <div className={styles.contentGeral}>
-            <BoxMessage />
-            <div className={styles.contentButton}>
-                <Fab color="primary" aria-label="add">
-                    <AddIcon />
-                </Fab>
+            <div className={styles.contentInput}>
+                <input type="text" />
+            </div>
+            <div className={styles.contentUserMessage}>
+                <BoxMessage />
             </div>
         </div>
     )
