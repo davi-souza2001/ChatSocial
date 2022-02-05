c3e85b8099831048584bf524e3fcdbdce4448da0
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-05 11:03:47 -0300

    up

--- src/service/context/ChatContext.tsx
+++ src/service/context/ChatContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 
-import { auth, database, ref, get, set, child } from '../../firebase/config';
+import { auth, database, ref, get, set, child, onValue } from '../../firebase/config';
 import useAuth from '../hook/useAuth';
 
 interface AuthContextProps {
@@ -18,38 +18,25 @@ const AuthContext = createContext<AuthContextProps>({});
 export function ChatProvider(props: any) {
   const { user } = useAuth()
   const [messageUserUnic, setMessageUserUnic] = useState({ name: 'Geral' })
-  const [messages, setMessages] = useState<Object[]>([])
   const [messageSend, setMessageSend] = useState('')
+  const [messages, setMessages] = useState<Object[]>([])
   const unicId = Math.floor(Date.now() * Math.random()).toString(36)
 
-  function sendMensage(){
+  function sendMensage() {
     const db = database
     set(ref(db, `chat/${messageUserUnic.name}/` + unicId), {
       mensage: messageSend,
       userSend: user?.email
     })
-    console.log('enviado')
   }
 
   async function checkChatExists() {
-    const dbRef = ref(database);
-    get(child(dbRef, `/chat/${messageUserUnic.name}`))
-      .then(async (snapshot) => {
-        if (snapshot.exists()) {
-          // console.log(snapshot.val())
-          const allUMessages = await snapshot.val();
-          // const messageList: Array<any> = [];
-          // for (let id in allUMessages) {
-          //   messageList.push({ id, ...allUMessages[id] });
-          // }
-          setMessages(allUMessages)
-        } else {
-          console.log('Não tem nada');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const dbRef = database
+    const starCountRef = ref(dbRef, 'chat/' + messageUserUnic.name)
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val()
+      console.log(data)
+    })
   }
 
   useEffect(() => {
