29ec32fd2a6d1ba3f59db634c9138fcc20beb29a
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-08 09:15:31 -0300

    up

--- src/components/MessageContent/index.tsx
+++ src/components/MessageContent/index.tsx
@@ -3,25 +3,19 @@ import UseChat from '../../service/hook/useChat';
 import styles from './MessageContent.module.scss';
 
 export default function index() {
-  const { sendMensage, setMessageSend, messages, messageUserUnic } = UseChat()
+  const { sendMensage, setMessageSend, messages } = UseChat()
   const { user } = UseAuth()
-
-  function renderMensages() {
-    if (messageUserUnic.name === 'Geral') {
-      return messages?.map((msg: any) => {
-        return (
-          <div className={msg.userSend == user?.email ? styles.textRight : styles.textLeft} key={msg.id}>
-            <p>{msg.mensage}</p>
-          </div>
-        )
-      })
-    }
-  }
 
   return (
     <div className={styles.chat}>
       <div className={styles.containerText}>
-        {renderMensages()}
+        {user?.email != '' && messages?.map((msg: any) => {
+          return (
+            <div className={msg.userSend == user?.email ? styles.textRight : styles.textLeft} key={msg.id}>
+              <p>{msg.mensage}</p>
+            </div>
+          )
+        })}
       </div>
       <div className={styles.input}>
         <i className="fas fa-plus"></i>
